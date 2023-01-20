f8d065a8067db1d0a639379a87c2446002d6fda9
ademclk <ademonurcelik@icloud.com>
2023-01-20 19:53:02 +0300

    Fixed error response

--- ceyehat/Controllers/ApiController.cs
+++ ceyehat/Controllers/ApiController.cs
@@ -1,6 +1,7 @@
 using ceyehat.Common.Http;
 using ErrorOr;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace ceyehat.Controllers;
 
@@ -9,10 +10,22 @@ public class ApiController : ControllerBase
 {
     protected IActionResult Problem(List<Error> errors)
     {
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+        
         HttpContext.Items[HttpContextItemKeys.Errors] = errors;
         
-        var firstError = errors.First();
-        
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error firstError)
+    {
         var statusCode = firstError.Type switch
         {
             ErrorType.Conflict => StatusCodes.Status409Conflict,
@@ -20,7 +33,21 @@ public class ApiController : ControllerBase
             ErrorType.NotFound => StatusCodes.Status404NotFound,
             _ => StatusCodes.Status500InternalServerError
         };
-        
+
         return Problem(statusCode: statusCode, title: firstError.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
     }
 }
