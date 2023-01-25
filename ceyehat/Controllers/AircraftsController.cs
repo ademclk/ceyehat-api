adf9b5de312e95204a6e1cfcd9618cc75d346eee
ademclk <ademonurcelik@icloud.com>
2023-01-26 02:08:23 +0300

    AircraftsController test and revised requests folder

--- ceyehat/Controllers/AircraftsController.cs
+++ ceyehat/Controllers/AircraftsController.cs
@@ -1,9 +1,13 @@
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ceyehat.Controllers;
 
 [Route("api/[controller]")]
 public class AircraftsController : ApiController
 {
-    
+    [HttpGet]
+    public IActionResult ListAircrafts()
+    {
+        return Ok(Array.Empty<string>());
+    }
 }
