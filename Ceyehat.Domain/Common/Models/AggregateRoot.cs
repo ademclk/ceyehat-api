10474659e19761119a2ca69a7f239ba44e8ea8d9
ademclk <ademonurcelik@icloud.com>
2023-01-30 01:11:35 +0300

    AggregateRoot implementation

---
+++ Ceyehat.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,9 @@
+namespace Ceyehat.Domain.Common.Models;
+
+public abstract class AggregateRoot<TId> : Entity<TId>
+    where TId : notnull
+{
+    protected AggregateRoot(TId id) : base(id)
+    {
+    }
+}
