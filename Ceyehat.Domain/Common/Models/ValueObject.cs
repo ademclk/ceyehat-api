b8d042e34fe925bfb5f15ad31a9d0375bc89e394
ademclk <ademonurcelik@icloud.com>
2023-01-30 00:50:28 +0300

    ValueObject implementation

---
+++ Ceyehat.Domain/Common/Models/ValueObject.cs
@@ -0,0 +1,41 @@
+namespace Ceyehat.Domain.Common.Models;
+
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var valueObject = (ValueObject)obj;
+
+        return GetEqualityComponents()
+            .SequenceEqual(valueObject.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject l, ValueObject r)
+    {
+        return Equals(l, r);
+    }
+
+    public static bool operator !=(ValueObject l, ValueObject r)
+    {
+        return !Equals(l, r);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x?.GetHashCode() ?? 0)
+            .Aggregate((x, y) => x ^ y);
+    }
+}
