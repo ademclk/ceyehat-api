1940276c41f83c3cbc27ba352e85ea3cbe8c56fb
ademclk <ademonurcelik@icloud.com>
2023-02-16 16:00:53 +0300

    Fixed issue #64 MediatR migration guide

--- Ceyehat.Application/DependencyInjection.cs
+++ Ceyehat.Application/DependencyInjection.cs
@@ -9,7 +9,9 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
 
         services.AddScoped(
             typeof(IPipelineBehavior<,>),
