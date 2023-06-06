703454c11b1ce5ecbe6205886b6df0f5796a30e5
ademclk <ademonurcelik@icloud.com>
2023-06-06 15:19:25 +0300

    Update register handler

--- Ceyehat.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
+++ Ceyehat.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -2,6 +2,7 @@ using Ceyehat.Application.Authentication.Common;
 using Ceyehat.Application.Common.Interfaces.Authentication;
 using Ceyehat.Application.Common.Interfaces.Persistence;
 using Ceyehat.Domain.Common.Errors;
+using Ceyehat.Domain.CustomerAggregate;
 using Ceyehat.Domain.CustomerAggregate.ValueObjects;
 using Ceyehat.Domain.UserAggregate;
 using Ceyehat.Domain.UserAggregate.ValueObjects;
@@ -15,11 +16,13 @@ public class RegisterCommandHandler :
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
     private readonly IUserRepository _userRepository;
+    private readonly ICustomerRepository _customerRepository;
 
-    public RegisterCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository)
+    public RegisterCommandHandler(IJwtTokenGenerator jwtTokenGenerator, IUserRepository userRepository, ICustomerRepository customerRepository)
     {
         _jwtTokenGenerator = jwtTokenGenerator;
         _userRepository = userRepository;
+        _customerRepository = customerRepository;
     }
 
     public async Task<ErrorOr<Token>> Handle(RegisterCommand command, CancellationToken cancellationToken)
@@ -30,13 +33,15 @@ public class RegisterCommandHandler :
         {
             return Errors.User.DuplicateEmail;
         }
-
+        
+        var customerResponse = await _customerRepository.GetCustomerByEmailAsync(command.Email);
+        
         var user = User.Create(
             command.FirstName,
             command.LastName,
             command.Email,
             command.Password,
-            CustomerId.CreateUnique());
+            customerResponse?.Id ?? CustomerId.CreateUnique());
 
         await _userRepository.AddUserAsync(user);
 
